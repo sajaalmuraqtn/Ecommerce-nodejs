ac895f3782309d25fa15cb37293ba9320b658232
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2024-01-06 18:15:54 +0300

    role edit

--- Src/Modules/Catagories/catagories.router.js
+++ Src/Modules/Catagories/catagories.router.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import * as CatagoriesController from './Controller/catagories.controller.js'
 import SubCatagoriesRouter from '../SubCatagories/subcatagories.router.js'
 import fileUpload, { fileValidation } from "../../Services/multer.js";
-import { auth } from "../../Middleware/auth.js";
+import { auth, roles } from "../../Middleware/auth.js";
 import { endPoint } from "./category.endpoint.js";
 import { asyncHandler } from "../../Services/errorHandling.js";
 import * as validators from "./category.validation.js";
@@ -11,7 +11,7 @@ import { validation } from "../../Middleware/validation.js";
 const router=Router();
 
 router.use('/:id/subCatagories',SubCatagoriesRouter);
-router.get('/',auth(endPoint.getall),asyncHandler(CatagoriesController.getCatagories));
+router.get('/',auth(Object.values(roles)),asyncHandler(CatagoriesController.getCatagories));
 router.get('/active',asyncHandler(CatagoriesController.getActiveCategory));
 router.get('/:id',validation(validators.getSpecificCategory),asyncHandler(CatagoriesController.getSpecificCategory));
 router.post('/create',auth(endPoint.create),fileUpload(fileValidation.image).array('image'),validation(validators.createCategory),asyncHandler(CatagoriesController.createCategory))
