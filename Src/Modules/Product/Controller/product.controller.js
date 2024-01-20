5983084c528b8c4b57237ac54ba4d26fadfe6c51
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2024-01-20 03:20:53 +0300

    filter

--- Src/Modules/Product/Controller/product.controller.js
+++ Src/Modules/Product/Controller/product.controller.js
@@ -6,46 +6,59 @@ import ProductModel from "../../../../DB/model/product.model.js";
 import { asyncHandler } from "../../../Services/errorHandling.js";
 import { pagination } from "../../../Services/pagination.js";
 
-export const getProduct=async(req,res,next)=>{
+export const getProduct = async (req, res, next) => {
+
     const { limit, skip } = pagination(req.query.page, req.query.limit);
-    const products=await ProductModel.find().limit(limit).skip(skip);
-    return res.status(201).json({message:'success',products});
+
+    let queryObj = {...req.query};
+    const execQuery = ['page','limit','skip','sort'];
+    execQuery.map((ele)=>{
+        delete queryObj[ele];
+    })
+    queryObj=JSON.stringify(queryObj);
+    queryObj=queryObj.replace(/\b(gt|gte|lt|lte|in|nin|eq|neq)\b/g,match=>`$${match}`);
+    queryObj=JSON.parse(queryObj);
+ 
+    const mongooseQuery= await ProductModel.find(queryObj).limit(limit).skip(skip);
+    const products =await mongooseQuery ;
+    const count=await ProductModel.estimatedDocumentCount();
+    return res.status(201).json({ message: 'success',page:products.length,total:count, products });
 }
 
-export const createProduct=asyncHandler( async(req,res,next)=>{
- 
-    const {price,discount,categoryId,subCategoryId,description}=req.body;
-    const name =req.body.name.toLowerCase(); 
-    if (await ProductModel.findOne({name}).select('name')) {
-        return next(new Error("product name already exist",{cause:409}));    
+export const createProduct = asyncHandler(async (req, res, next) => {
+
+    const { price, discount, categoryId, subCategoryId, description } = req.body;
+    const name = req.body.name.toLowerCase();
+    if (await ProductModel.findOne({ name }).select('name')) {
+        return next(new Error("product name already exist", { cause: 409 }));
     }
-     req.body.slug=slugify(name); 
-    const checkCategory=await CategoryModel.findById(categoryId);
-    if(!checkCategory){
-        return next(new Error("category not found",{cause:404}));     
+    req.body.slug = slugify(name);
+    const checkCategory = await CategoryModel.findById(categoryId);
+    if (!checkCategory) {
+        return next(new Error("category not found", { cause: 404 }));
     }
-    const checkSubCategory=await SubCategoryModel.findById(subCategoryId);
-    if(!checkSubCategory){
-        return next(new Error("sub category not found",{cause:404}));      
+    const checkSubCategory = await SubCategoryModel.findById(subCategoryId);
+    if (!checkSubCategory) {
+        return next(new Error("sub category not found", { cause: 404 }));
     }
-     req.body.finalPrice=(price - (price*(discount||0)/100)).toFixed(2);
-     const{secure_url,public_id}=await cloudinary.uploader.upload(req.files.mainImage[0].path,{folder:`${process.env.APP_NAME}/product/mainImage`});
-    req.body.mainImage={secure_url,public_id};
-    req.body.subImages=[];
+    req.body.finalPrice = (price - (price * (discount || 0) / 100)).toFixed(2);
+    const { secure_url, public_id } = await cloudinary.uploader.upload(req.files.mainImage[0].path, { folder: `${process.env.APP_NAME}/product/mainImage` });
+    req.body.mainImage = { secure_url, public_id };
+    req.body.subImages = [];
     for (const file of req.files.subImages) {
-        const{secure_url,public_id}=await cloudinary.uploader.upload(file.path,{folder:`${process.env.APP_NAME}/product/subImages`});     
-        req.body.subImages.push({secure_url,public_id});
+        const { secure_url, public_id } = await cloudinary.uploader.upload(file.path, { folder: `${process.env.APP_NAME}/product/subImages` });
+        req.body.subImages.push({ secure_url, public_id });
     }
-    req.body.createdBy=req.user._id;
-    req.body.updatedBy=req.user._id;
-    const product=await ProductModel.create(req.body);
+    req.body.createdBy = req.user._id;
+    req.body.updatedBy = req.user._id;
+    const product = await ProductModel.create(req.body);
     if (!product) {
-        return next(new Error("error while creating product",{cause:400}));      
+        return next(new Error("error while creating product", { cause: 400 }));
     }
-    return res.status(201).json({message:'success',product}); 
+    return res.status(201).json({ message: 'success', product });
 })
 
-export const getProductWithCategory=async(req,res,next)=>{
-    const products=await ProductModel.find({categoryId:req.params.categoryId});
-    return res.status(201).json({message:'success',products});
+export const getProductWithCategory = async (req, res, next) => {
+    const products = await ProductModel.find({ categoryId: req.params.categoryId });
+    return res.status(201).json({ message: 'success', products });
 }
